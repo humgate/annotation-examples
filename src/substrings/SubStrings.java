0bd4c42b1d28db733f93afc551ea1a8c5fecb000
Pavel Yanovsky <humgate@gmail.com>
2022-08-04 23:05:07 +0600

    String decoder preliminary solution

--- src/substrings/SubStrings.java
+++ src/substrings/SubStrings.java
@@ -6,8 +6,11 @@ import java.util.HashMap;
 
 public class SubStrings {
     public static String s = "taaareexeraa";
+    public static String s1 = "3[a2[x]2[b]]";
+
 
     /**
+     * Description from leetcode:
      * Given a string s, sort it in decreasing order based on the frequency of the characters.
      * The frequency of a character is the number of times it appears in the string. Return the sorted string.
      * If there are multiple answers, return any of them.
@@ -34,7 +37,55 @@ public class SubStrings {
         return b.toString();
     }
 
+    /**
+    * Description from leetcode:
+    * Given an encoded string, return its decoded string.
+    * The encoding rule is: k[encoded_string], where the encoded_string inside the square brackets is
+    * being repeated exactly k times. Note that k is guaranteed to be a positive integer.
+    * You may assume that the input string is always valid; there are no extra white spaces, square brackets are
+    * well-formed, etc. Furthermore, you may assume that the original data does not contain any digits and
+    * that digits are only for those repeat numbers, k. For example, there will not be input like 3a or 2[4].
+    */
+    public static String decodeString(String s) {
+        //3[a2[x]2[s]]
+        if (s.isEmpty()) return "";
+
+//        if (s.matches("^\\d+\\[[a-z]+\\]{2,}.*")) {
+//            int i1 = s.indexOf("[");
+//            int i2 = s.indexOf("]");
+//            int repeat = Integer.parseInt(s.substring(0, i1));
+//            return s.substring(i1 + 1, i2).repeat(repeat);
+//        }
+
+
+        if (s.matches("^\\d+\\[[a-z]+\\].*")) {
+            int i1 = s.indexOf("[");
+            int i2 = s.indexOf("]");
+            int repeat = Integer.parseInt(s.substring(0, i1));
+            return s.substring(i1 + 1, i2).repeat(repeat) + decodeString(s.substring(1));
+        }
+
+        if (s.matches("^\\d+\\[[a-z]+.+")) {
+            String stemp = decodeString(s.substring(1));
+            int i1 = s.indexOf("[");
+            int i2 = s.substring(i1 + 1).indexOf("[");
+            int i3 = s.indexOf("]");
+            int repeat = Integer.parseInt(s.substring(0, i1));
+            String stemp2 = s.replace(s.substring(i1 + 2, i3 + 1), stemp);
+            i3 = stemp2.indexOf("]");
+            String stemp3 = stemp2.substring(i1 + 1, i3);
+
+            return stemp3.repeat(repeat);
+        }
+
+           return decodeString(s.substring(1));
+    }
+
+
+
+
     public static void main(String[] args) {
-        System.out.println(frequencySort(s));
+        String s2 = decodeString(s1);
+        System.out.println(s2);
     }
 }
